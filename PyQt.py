ed6b584b9ba6548072c2ea95e45b7a56138da560
gagnonsilas <you@example.com>
2020-02-13 22:49:58 -0800

    Created class and made button to quit

--- PyQt.py
+++ PyQt.py
@@ -1,12 +1,29 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 class Window(QtWidgets.QMainWindow):
     
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
-        self.setWindowIcon()
+        self.setWindowTitle("PyQt testing")
+        self.home()
+        
+    def home(self):
+        btn = QtWidgets.QPushButton("Quit", self)
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn.resize(100, 100)
+        btn.move(100, 100)
+        
+        self.show()
+        
+        
+    
+def run():        
 
+    app = QtWidgets.QApplication(sys.argv)
+    GUI = Window()
 
-sys.exit(app.exec_())
+    sys.exit(app.exec_())
+    
+run()
