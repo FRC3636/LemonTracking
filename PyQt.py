a1ebeb66a5b87d28aea40ec83ec973e25c1ab15d
gagnonsilas <gagnon.silas@gmail.com>
2020-02-14 20:10:03 -0800

    "Made main window stuff class"

--- PyQt.py
+++ PyQt.py
@@ -1,33 +1,13 @@
 import sys
 from PyQt5 import QtWidgets, QtCore
-
-class Window(QtWidgets.QMainWindow):
-    
-    def __init__(self):
-        super(Window, self).__init__()
-        self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle("PyQt testing")
-        self.home()
-        
-    def home(self):
-        btn = QtWidgets.QPushButton("Quit", self)
-        btn.clicked.connect(self.close_function)
-        btn.resize(100, 100)
-        btn.move(100, 100)
-        
-        self.show()
-    
-    def close_function(self):
-    	print("yay i did a function thing")
-    	sys.exit()
-        
+import PyQt_Window as Qt
         
     
 def run():        
 
     app = QtWidgets.QApplication(sys.argv)
-    GUI = Window()
+    GUI = Qt.Window()
 
     sys.exit(app.exec_())
-    
+
 run()
