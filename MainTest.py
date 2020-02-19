23b4e6ef4a25859d9befc35a42a91190d67b9a70
gagnonsilas <gagnon.silas@gmail.com>
2020-02-18 19:36:56 -0800

    The MainTest.py has been improved with a filter using the average of img crop to the "object" and compares it to what a lemon img should be. More info is at the start of the script

--- MainTest.py
+++ MainTest.py
@@ -4,18 +4,29 @@ import math
 import threading
 from networktables import NetworkTables
 
-cap = cv2.VideoCapture(1)
+'''
+Note for other coders
+The calibration need to be tuned for other backgrounds
+The yellow boxes are the calibration objects (the one with the largest area
+Prints the statements of the calibration object
+Blue boxes are the tracked object
+'''
+
+cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('http://10.36.36.216:4747/mjpegfeed?640x480')
 
 # Rescaling function
 def rescale_frame(res, percent=75):
-            width = int(res.shape[1] * percent/ 100)
-            height = int(res.shape[0] * percent/ 100)
-            dim = (width, height)
-            return cv2.resize(res, dim, interpolation =cv2.INTER_AREA)
+    width = int(res.shape[1] * percent/ 100)
+    height = int(res.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv2.resize(res, dim, interpolation =cv2.INTER_AREA)
 
 # Lemon finder function
 def lemonFinder(frame):
+
+    # to avoid error
+    crop = frame
     
     # Converting image to hs
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -25,8 +36,8 @@ def lemonFinder(frame):
     
     
     # Define range of yellow  color in HSV
-    lower_yellow = np.array([21,70,15]) # old: [21,60,15]
-    upper_yellow = np.array([35,255,255]) # old: [32,255,255]
+    lower_yellow = np.array([21, 90, 15])  # old: [21,60,15]
+    upper_yellow = np.array([35, 255, 255])  # old: [32,255,255]
 
     # Find yellow
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
@@ -56,50 +67,66 @@ def lemonFinder(frame):
     for cont in contours:
         # Find boxes
         x, y, w, h = cv2.boundingRect(cont)
-        
-        # Draw boxes large objects by using area to remove unwanted objects (like hair)
-        if (w * h) > 500:
-            balls += 1
-            
-            # centers
-            centerX = x + (w / 2)
-            centerY = y + (h / 2)
-            
-            # Finding closest ball
-            if (w * h) > trackA:
-                trackX = x
-                trackY = y
-                trackA = (w * h)
-                trackW = w
-                trackH = h
-                
-            # Draw rectangle
-            cv2.rectangle(frame, (x, y), (x + w,y + h), (200,100,100), 2)
-            
-            # Add lemon position text
-            cv2.putText(frame, 'Ball' + str(balls), (x + 5, y + (h/4)), 1, 1, 255, 2)
-            cv2.putText(frame, 'Pos:', (x + 5, y + 2 * (h/4)), 1, 1, 255, 2)
-            cv2.putText(frame, '(' + str(x) + ', ' + str(y) + ')', (x + 5, y + 3 * (h/4)), 1, 1, 255, 2)
-        
+
+        # centers
+        centerX = x + (w / 2)
+        centerY = y + (h / 2)
+
+        # Finding closest ball
+        if (w * h) > trackA:
+            trackX = x
+            trackY = y
+            trackA = (w * h)
+            trackW = w
+            trackH = h
+
+        # for calibrating the tracker
+        # crop image
+        xpos = trackX + trackW
+        ypos = trackY + trackH
+        crop = frame[trackY:ypos, trackX:xpos]
+
+        # average the crop color
+        average1 = np.mean(crop, axis=0)
+        average2 = np.mean(average1, axis=0)
+
+        # Draw rectangle
+        cv2.rectangle(frame, (trackX, trackY), (trackX + trackW, trackY + trackH), (10, 255, 255), 2)
+
+        print(average2[0] > 30 and average2[0] < 80, average2[1] > 70 and average2[1] < 115, average2[2] > 65 and average2[2] < 160)
+
+        print(average2[2])
+
+        # actual tracking
         # crop image
         xpos = x + w
         ypos = y + h
-        crop = hsv[y:ypos,x:xpos]
-        
-        # Blur hsv
-        crophsv = cv2.GaussianBlur(crop, (41, 41), 0)
+        crop = frame[y:ypos, x:xpos]
 
-        # Find remask the crop to confirm the ball
-        mask = cv2.inRange(crophsv, lower_yellow, upper_yellow)
-        
-        #plez add code to confirm ball
+        # average the crop color
+        average1 = np.mean(crop, axis=0)
+        average2 = np.mean(average1, axis=0)
+
+        # use average to confirm ball
+        if average2[0] > 30 and average2[0] < 80 and average2[1] > 70 and average2[1] < 115 and average2[2] > 65 and average2[2] < 160:
+
+            # ball count
+            balls += 1
+
+            # Draw rectangle
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 100, 100), 2)
+
+            # Add lemon position text
+            cv2.putText(frame, 'Ball' + str(balls), (x + 5, int(y + (h/4))), 1, 1, 255, 2)
+            cv2.putText(frame, 'Pos:', (x + 5, int(y + 2 * (h/4))), 1, 1, 255, 2)
+            cv2.putText(frame, '(' + str(x) + ', ' + str(y) + ')', (x + 5, int(y + 3 * (h/4))), 1, 1, 255, 2)
             
             
     # resize the image
     # frame = rescale_frame(frame, 300)
     
     # Return frame and mask
-    return(frame, mask)
+    return(frame)
 
 
 
@@ -109,11 +136,10 @@ while(True):
     ret, frame = cap.read()
  
     # Run the lemon finder function
-    frame, crop = lemonFinder(frame)    
+    frame = lemonFinder(frame)
     
     # Show the frames
     cv2.imshow('coloredCircles', frame)
-    cv2.imshow('crop', crop)
     
     # If escape key is pressed exit while loop
     if cv2.waitKey(1) & 0xFF == 27:
