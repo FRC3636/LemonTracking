088fb93406bfb00cdb7335f8ae00c477f61b203d
gagnonsilas <gagnon.silas@gmail.com>
2020-02-21 17:48:40 -0800

    Imporoved Tracking (almost perfict). Uses average hsv color from cicular mask to filter out noise

--- MainTest.py
+++ MainTest.py
@@ -12,6 +12,23 @@ Blue boxes are the tracked object
 
 cap = cv2.VideoCapture(0)
 
+def createCircle(w, h):
+    # creates distance img for creating circular crops
+    X, Y = np.ogrid[:w, :h]
+    CenterX, CenterY = w/2, h/2
+    img = ((X - CenterX) ** 2 + (Y - CenterY) ** 2)
+
+    return img
+
+def centerCrop(img, w, h):
+    # crops around center
+    maxY, maxX =  img.shape[0], img.shape[1]
+    x1, y1 = int(maxX/2 - w/2), int(maxY/2 - h/2)
+    x2, y2 = int(maxX/2 + w/2), int(maxY/2 + h/2)
+    crop = img[y1:y2, x1:x2]
+    return crop
+
+
 # Rescaling function
 def rescale_frame(res, percent=75):
     width = int(res.shape[1] * percent/ 100)
@@ -20,23 +37,17 @@ def rescale_frame(res, percent=75):
     return cv2.resize(res, dim, interpolation =cv2.INTER_AREA)
 
 # Lemon finder function
-def lemonFinder(frame):
+def lemonFinder(frame, dist_img):
 
     # to avoid error
     crop = frame
-    
-    # Converting image to hs
+    # Converting image to hsv and blur
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    # Blur hsv
     hsv = cv2.GaussianBlur(hsv, (41, 41), 0) 
-    
-    
-    # Define range of yellow  color in HSV
-    lower_yellow = np.array([21, 90, 15])  # old: [21,60,15]
-    upper_yellow = np.array([35, 255, 255])  # old: [32,255,255]
 
-    # Find yellow
+    # Define range of yellow  color in HSV and mask img
+    lower_yellow = np.array([20, 55, 15])  # old: [21,60,15]
+    upper_yellow = np.array([35, 255, 255])  # old: [32,255,255]
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
     
     # Cleaning up mask
@@ -44,68 +55,36 @@ def lemonFinder(frame):
     mask = cv2.erode(mask, kernel, iterations=1)
     mask = cv2.dilate(mask, kernel, iterations=1)
 
-
-    # Bounding boxes
-    # For drawing img
-    # Find contours
+    # Find bounding Boxes
     contours, hierarchy = cv2.findContours(mask, 1, 2)
     
     # Create variable for number of balls
     balls = 0
-
-    # Variables for picking closest ball
-    trackX = 0
-    trackY = 0
-    trackW = 2
-    trackH = 2
-    trackA = 1
     
     # Run for every contour
     for cont in contours:
         # Find boxes
         x, y, w, h = cv2.boundingRect(cont)
 
-        # centers
-        centerX = x + (w / 2)
-        centerY = y + (h / 2)
-
-        # Finding closest ball
-        if (w * h) > trackA:
-            trackX = x
-            trackY = y
-            trackA = (w * h)
-            trackW = w
-            trackH = h
-
-        # for calibrating the tracker
-        # crop image
-        xpos = trackX + trackW
-        ypos = trackY + trackH
-
-        crop = frame[trackY:ypos, trackX:xpos]
-        hsvCrop = hsv[y:ypos, x:xpos]
-
-        # average the crop color
-        average1 = np.mean(hsvCrop, axis=0)
-        average2 = np.mean(average1, axis=0)
-
-        # Draw rectangle
-        #cv2.rectangle(frame, (trackX, trackY), (trackX + trackW, trackY + trackH), (10, 255, 255), 2)
-
-        print(average2, trackA)
-
         # actual tracking
         # crop image
         xpos = x + w
         ypos = y + h
-        hsvCrop = hsv[y:ypos, x:xpos]
+        crop = hsv[y:ypos, x:xpos]
+        circleMask = centerCrop(dist_img, w, h) < (w /2) ** 2
+
+        circleColor = crop[circleMask]
 
         # average the crop color
-        average1 = np.mean(hsvCrop, axis=0)
-        average2 = np.mean(average1, axis=0)
+        average = np.mean(circleColor, axis=0)
+
+        # Draw rectangle
+        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 200, 200), 2)
+
+        print(average, (w * h))
 
         # use average to confirm ball
-        if average2[0] > 20 and average2[0] < 70 and average2[1] > 100 and average2[1] < 180 and average2[2] > 110 and average2[2] < 220:
+        if average[0] > 22 and average[0] < 30 and average[1] > 65 and average[1] < 175 and average[2] > 100 and average[2] < 175:
 
             # ball count
             balls += 1
@@ -114,18 +93,19 @@ def lemonFinder(frame):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 100, 100), 2)
 
             # Add lemon position text
-            cv2.putText(frame, 'Ball' + str(balls), (x + 5, int(y + (h/4))), 1, 1, 255, 2)
+            cv2.putText(frame, 'Lemon' + str(balls), (x + 5, int(y + (h/4))), 1, 1, 255, 2)
             cv2.putText(frame, 'Pos:', (x + 5, int(y + 2 * (h/4))), 1, 1, 255, 2)
             cv2.putText(frame, '(' + str(x) + ', ' + str(y) + ')', (x + 5, int(y + 3 * (h / 4))), 1, 1, 255, 2)
-            
-            
+
+
     # resize the image
     frame = rescale_frame(frame, 300)
     
     # Return frame and mask
-    return(frame)
+    return(frame, crop)
 
 
+dist_img = createCircle(480, 640)
 
 while(True):
 
@@ -133,16 +113,16 @@ while(True):
     ret, frame = cap.read()
  
     # Run the lemon finder function
-    frame= lemonFinder(frame)
+    frame, crop = lemonFinder(frame, dist_img)
     
     # Show the frames
+    cv2.imshow('crop', crop)
     cv2.imshow('coloredCircles', frame)
     
     # If escape key is pressed exit while loop
     if cv2.waitKey(1) & 0xFF == 27:
         break
-        
-    
+
 
 # Close the windows
 cap.release()
